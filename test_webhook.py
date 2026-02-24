55cbfc2f295420d80d75e6e714af29853e0d41b0
NayeemHossenJim <nayeemhossenjim@gmail.com>
2026-02-24 12:09:12 +0600

    Update webhook URL in app and test files

--- test_webhook.py
+++ test_webhook.py
@@ -5,7 +5,7 @@ user_message = "Can you tell me about black holes in 3-4 lines"
 
 request_message = {"message": user_message}
 
-url = "http://localhost:5678/webhook-test/ca402f54-1679-474d-b5c6-f2e309659f09"
+url = "http://localhost:5678/webhook/ca402f54-1679-474d-b5c6-f2e309659f09"
 
 response = requests.post(url, json=request_message)
 
